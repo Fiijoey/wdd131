0cbcee507119e825bcffd9f23bb34ccbcf61621d
Fiijoey <kofimensah3730@gmail.com>
2024-12-16 23:06:03 +0000

    Projects pages are done both the mobile and desktop.

--- project/scripts/project.js
+++ project/scripts/project.js
@@ -1,109 +1,39 @@
-const temples = [
+const projects = [
   {
-    templeName: "Aba Nigeria",
-    location: "Aba, Nigeria",
-    dedicated: "2005, August, 7",
-    area: 11500,
-    imageUrl:
-      "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/aba-nigeria/400x250/aba-nigeria-temple-lds-273999-wallpaper.jpg",
+    projectName: "Temple album project",
+    dateis: "2024, November, 7",
+    image: "images/image1.png",
+    projectUrl:
+    "https://fiijoey.github.io/wdd131/filtered-temples.html"
   },
   {
-    templeName: "Manti Utah",
-    location: "Manti, Utah, United States",
-    dedicated: "1888, May, 21",
-    area: 74792,
-    imageUrl:
-      "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/manti-utah/400x250/manti-temple-768192-wallpaper.jpg",
+    projectName: "Places Information project",
+    dateis: "2024, November, 21",
+    image: "images/image5.png",
+    projectUrl:
+    "https://fiijoey.github.io/wdd131/place.html"
   },
   {
-    templeName: "Payson Utah",
-    location: "Payson, Utah, United States",
-    dedicated: "2015, June, 7",
-    area: 96630,
-    imageUrl:
-      "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/payson-utah/400x225/payson-utah-temple-exterior-1416671-wallpaper.jpg",
-  },
-  {
-    templeName: "Yigo Guam",
-    location: "Yigo, Guam",
-    dedicated: "2020, May, 2",
-    area: 6861,
-    imageUrl:
-      "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/yigo-guam/400x250/yigo_guam_temple_2.jpg",
-  },
-  {
-    templeName: "Washington D.C.",
-    location: "Kensington, Maryland, United States",
-    dedicated: "1974, November, 19",
-    area: 156558,
-    imageUrl:
-      "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/washington-dc/400x250/washington_dc_temple-exterior-2.jpeg",
-  },
-  {
-    templeName: "Lima Perú",
-    location: "Lima, Perú",
-    dedicated: "1986, January, 10",
-    area: 9600,
-    imageUrl:
-      "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/lima-peru/400x250/lima-peru-temple-evening-1075606-wallpaper.jpg",
-  },
-  {
-    templeName: "Mexico City Mexico",
-    location: "Mexico City, Mexico",
-    dedicated: "1983, December, 2",
-    area: 116642,
-    imageUrl:
-      "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/mexico-city-mexico/400x250/mexico-city-temple-exterior-1518361-wallpaper.jpg",
-  },
-  {
-    templeName: "Accra Ghana",
-    location: "Accra, Ghana",
-    dedicated: "2004, January, 11",
-    area: 17500,
-    imageUrl:
-      "https://churchofjesuschristtemples.org/assets/img/temples/accra-ghana-temple/accra-ghana-temple-13760-main.jpg",
-  },
-  {
-    templeName: "Madrid Spain",
-    location: "Madrid, Spain",
-    dedicated: "1999, March, 19-21",
-    area: 45800,
-    imageUrl:
-      "https://churchofjesuschristtemples.org/assets/img/temples/_temp/056-Madrid-Spain-Temple.jpg",
-  },
-  {
-    templeName: "Bogota Colombia",
-    location: "Bogota, Colombia",
-    dedicated: "1999, April, 24-26",
-    area: 53500,
-    imageUrl:
-      "https://churchofjesuschristtemples.org/assets/img/temples/bogota-colombia-temple/bogota-colombia-temple-7733-main.jpg",
-  },
+    projectName: "Portfolio website project",
+    dateis: "2024, November, 7",
+    image: "images/image6.png",
+    projectUrl:
+    "https://fiijoey.github.io/wdd131/project/home.html"
+  }
+ 
 ];
 
-const templeCardsContainer = document.getElementById("temple-cards");
-temples.forEach((temple) => {
-  const templeCard = document.createElement("div");
-  templeCard.className = "temple-card";
+const projectCardsContainer = document.getElementById("project-cards");
+projects.forEach((project) => {
+  const projectCard = document.createElement("div");
+  projectCard.className = "project-card";
 
-  templeCard.innerHTML = ` 
-            <h2>${temple.templeName}</h2>
-            <p><strong>Location:</strong> ${temple.location}</p>
-            <p><strong>Dedicated:</strong> ${temple.dedicated}</p>
-            <p><strong>Area:</strong> ${temple.area} sq ft</p>
-            <img src="${temple.imageUrl}" alt="${temple.templeName}" loading="lazy"> 
+  projectCard.innerHTML = ` 
+            <h2>${project.projectName}</h2>
+            <p><strong>Date:</strong> ${project.dateis}</p>
+            <a href="${project.projectUrl}"><img src="${project.image}" alt="${project.projectName}" loading="lazy"> </a>
+            
     `;
 
-  templeCardsContainer.appendChild(templeCard);
+  projectCardsContainer.appendChild(projectCard);
 });
-
-const date = new Date();
-const form = new Intl.DateTimeFormat("en-us", {
-  dateStyle: "full",
-  timeStyle: "short",
-});
-var year = date.getFullYear();
-var modifiedt = form.format(new Date(document.lastModified));
-
-document.getElementById("currentyear").innerHTML = year;
-document.getElementById("lastModified").innerHTML = modifiedt;
